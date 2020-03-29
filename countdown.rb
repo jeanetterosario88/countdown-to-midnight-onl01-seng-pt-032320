baebb3a775e65508885ccf1751e68db296f80dc5
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-03-29 15:05:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown (x)
+  until x == 0
+  puts "#{x} SECOND(S)!"
+  x -= 1
+  if x == 0
+  return "HAPPY NEW YEAR!"
+  end
+end
+
+def countdown_with_sleep (x)
+  until x == 0
+  puts "#{x} SECOND(S)!"
+  sleep 1
+  x -= 1
+  if x == 0
+  return "HAPPY NEW YEAR!"
+  end
 end
